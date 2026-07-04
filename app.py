0541c314c36012d1b26ac990b1fe2b0d4a49c44c
Luciano <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2026-07-04 20:30:41 -0300

    feat: Añadir funcionalidad para registrar y gestionar productos en el sistema de inventario

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from modulos.conexion import conectar_db
+from modulos.registrar_producto import crear_producto
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/prueba_conexion")
 def test_conexion():
     try:
         conexion = conectar_db()
@@ -14,6 +15,39 @@ def test_conexion():
         return f"¡Conectado con éxito! Versión de MySQL: {version[0]}"
     except Exception as e:
         return f"Error en la conexión: {e}"
+    
+@app.route("/")
+def index():
+    render_template("index.html")
+
+@app.route("/productos")
+def productos():
+    return render_template("productos.html")
+
+@app.route("/productos/crear", methods=["GET", "POST"])
+def registrar_producto():
+    if request.method == "POST":
+        nombre_producto = request.form["nombre_producto"]
+        precio_producto = request.form["precio_producto"]
+        stok_actual_producto = request.form["stok_actual_producto"]
+        stok_minimo_producto = request.form["stok_minimo_producto"]
+        categoria_producto = request.form["idcategorias"]
+        
+        crear_producto(nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto)
+        return redirect("/productos")
+    
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM categorias;")
+    lista_categorias = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+    
+    return render_template("crear.html", categorias= lista_categorias)
+
+@app.route("/productos/editar/<int:id>", methods=["GET","POST"])
+def editar_producto():
+    return render_template("editar.html") 
     
 if __name__ == "__main__":
     app.run(debug=True)
