eff1ff161430de6bac3e331ee51027fc7eb525fa
Luciano <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2026-07-05 17:27:38 -0300

    feat: Refactor product management by consolidating database operations into dedicated functions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from modulos.conexion import conectar_db
-from modulos.registrar_producto import crear_producto
+from modulos.comandos_db_producto import sql_leer_productos, sql_leer_producto, sql_leer_categorias, sql_crear_producto, sql_actualizar_producto, sql_eliminar_producto
 app = Flask(__name__)
 
 
@@ -18,22 +18,18 @@ def test_conexion():
     
 @app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/productos")
 def productos():
     
-    conexion = conectar_db()
-    cursor = conexion.cursor()
-    cursor.execute("SELECT p.idproductos, p.nombre, p.precio, p.stock_actual, c.nombre AS categoria FROM productos AS p JOIN categorias AS c ON  p.idcategorias = c.idcategorias;")
-    lista_productos = cursor.fetchall()
-    cursor.close()
-    conexion.close()
+    lista_productos = sql_leer_productos()
     
     return render_template("productos.html", productos = lista_productos)
 
 @app.route("/productos/crear", methods=["GET", "POST"])
 def registrar_producto():
+    """Recibe la informacion enviada por el formulario"""
     if request.method == "POST":
         nombre_producto = request.form["nombre_producto"]
         precio_producto = request.form["precio_producto"]
@@ -41,24 +37,16 @@ def registrar_producto():
         stok_minimo_producto = request.form["stok_minimo_producto"]
         categoria_producto = request.form["idcategorias"]
         
-        crear_producto(nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto)
+        sql_crear_producto(nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto)
         return redirect("/productos")
     
-    conexion = conectar_db()
-    cursor = conexion.cursor()
-    """Busca todas las categorias de los productos"""
-    cursor.execute("SELECT * FROM categorias;")
-    lista_categorias = cursor.fetchall()
-    cursor.close()
-    conexion.close()
+    """Carga el formulario y las opciones de categorias"""
+    lista_categorias = sql_leer_categorias()
     
     return render_template("crear_producto.html", categorias= lista_categorias)
 
 @app.route("/productos/editar/<int:id>", methods=["GET","POST"])
 def editar_producto(id):
-    conexion = conectar_db()
-    cursor = conexion.cursor()
-    
     if request.method == "POST":
         """Recibo los valores enviados por el formulario"""
         nombre_producto = request.form["nombre_producto"]
@@ -68,39 +56,20 @@ def editar_producto(id):
         categoria_producto = request.form["idcategorias"]
         
         """Ahora voy a actualizar los datos"""
-        try:
-            cursor.execute("UPDATE productos SET nombre = %s, precio = %s, stock_actual = %s, stock_minimo = %s, idcategoria = %s WHERE idproductos = %s;", (nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto, id))
-            conexion.commit()
+        if sql_actualizar_producto(nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto, id) == True:
             return redirect("/productos")
-        except Exception as e:
-            """En el caso de haber un error deshace los cambios"""
-            conexion.rollback()
-        cursor.close()
-        conexion.close()
-        return "Hubo un error al guardar los datos", 500
+        else:
+            error = "Hubo un error al guardar los datos"
+            return error, 500
         
-    """Busca la informacion del producto correspondiente"""
-    cursor.execute("SELECT p.idproductos, p.nombre, p.precio, p.stock_actual, p.stock_minimo, c.nombre AS categoria FROM productos AS p WHERE p.idproductos = %s;", (id,))
-    producto = cursor.fetchone()
-    cursor.execute("SELECT * FROM categorias;")
-    lista_categorias = cursor.fetchall()
-    cursor.close()
-    conexion.close()
+    producto = sql_leer_producto(id)
+    lista_categorias = sql_leer_categorias()
+    
     return render_template("editar_producto.html", producto = producto, categorias = lista_categorias) 
 
 @app.route("/productos/eliminar/<int:id>")
 def eliminar_producto(id):
-    conexion = conectar_db()
-    cursor = conexion.cursor()
-    """Intenta borrar el producto seleccionado"""
-    try:    
-        cursor.execute("DELETE FROM productos WHERE idproductos = %s;", (id,))
-        conexion.commit()
-    except Exception as e:
-        """En el caso de haber un error deshace los cambios"""
-        conexion.rollback()
-    cursor.close()
-    conexion.close()
+    sql_eliminar_producto(id)
     return redirect("/productos")
 
 
---
+++ modulos/comandos_db_producto.py
@@ -0,0 +1,78 @@
+from modulos.conexion import conectar_db
+
+def sql_leer_productos():
+    """Lee la tabla de productos y devuele el listado"""
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    
+    cursor.execute("SELECT p.idproductos, p.nombre, p.precio, p.stock_actual, c.nombre AS categoria FROM productos AS p JOIN categorias AS c ON  p.idcategorias = c.idcategorias;")
+    lista_productos = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+    return lista_productos
+
+def sql_leer_producto(id):
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    
+    """Busca la informacion del producto correspondiente"""
+    cursor.execute("SELECT idproductos, nombre, precio, stock_actual, stock_minimo, idcategorias FROM productos WHERE idproductos = %s;", (id,))
+    producto = cursor.fetchone()
+    cursor.close()
+    conexion.close()
+    return producto
+
+def sql_leer_categorias():
+    """Lee la tabla de cateogias y devuelve el listado"""
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    
+    """Busca todas las categorias de los productos"""
+    cursor.execute("SELECT * FROM categorias;")
+    lista_categorias = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+    return lista_categorias
+
+def sql_crear_producto(nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto):
+    """Registra un nuevo producto"""
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    
+    sql = "INSERT INTO productos (nombre, precio, stock_actual, stock_minimo, idcategorias) VALUES (%s, %s, %s, %s, %s)"
+    valores = (nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto)
+    cursor.execute(sql, valores)
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+    
+def sql_actualizar_producto(nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto, id):
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    
+    try:
+        cursor.execute("UPDATE productos SET nombre = %s, precio = %s, stock_actual = %s, stock_minimo = %s, idcategorias = %s WHERE idproductos = %s;", (nombre_producto, precio_producto, stok_actual_producto, stok_minimo_producto, categoria_producto, id))
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+        return True
+    except Exception as e:
+        """En el caso de haber un error deshace los cambios"""
+        conexion.rollback()
+        cursor.close()
+        conexion.close()
+        print("❌ ERROR EN UPDATE:", e)
+        return False
+
+def sql_eliminar_producto(id):
+    conexion = conectar_db()
+    cursor = conexion.cursor()
+    """Intenta borrar el producto seleccionado"""
+    try:    
+        cursor.execute("DELETE FROM productos WHERE idproductos = %s;", (id,))
+        conexion.commit()
+    except Exception as e:
+        """En el caso de haber un error deshace los cambios"""
+        conexion.rollback()
+    cursor.close()
+    conexion.close()
